b0dea1b224a6734f328971bf756480b59565c58a
Arnur Kupanov <ncase01@gmail.com>
2020-05-28 15:56:22 +0600

    update

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,33 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+const cleanStr = (str) =>
+  str.replace(/[^\w]/g, '').toLowerCase().split('').sort().join('');
+
+const anagrams = (strA, strB) => cleanStr(strA) === cleanStr(strB);
+
+// function buildCharMap(str) {
+//   const charMap = {};
+
+//   for (const char of str.replace(/[^\w]/g, '').toLowerCase()) {
+//     charMap[char] = charMap[char]++ || 1;
+//   }
+
+//   return charMap;
+// }
+
+// function anagrams(stringA, stringB) {
+//   const aCharMap = buildCharMap(stringA);
+//   const bCharMap = buildCharMap(stringB);
+
+//   if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length)
+//     return false;
+
+//   for (const char in aCharMap) {
+//     if (aCharMap[char] !== bCharMap[char]) return false;
+//   }
+
+//   return true;
+// }
 
 module.exports = anagrams;
